d40bce1ba7a2832f8034d78b76ca12667595b68a
israelias <61286823+israelias@users.noreply.github.com>
2022-12-11 03:40:06 +0800

    ci: resolve env vars for site config

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,13 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'af420236cda942f8bc524c9b78789f7e',
+  rootNotionPageId: process.env.NEXT_PUBLIC_ROOT_ID,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'f0ff58a7-8b03-41b2-804f-738f211d1fe6',
-  //"1a2baa72-2b71-44d5-88a9-0b4255eb4077",
-
+  rootNotionSpaceId: process.env.NEXT_PUBLIC_SPACE_ID,
   // basic site info (required)
-  name: 'Source 23',
+  name: 'fwd-23',
   domain: 'israelias',
   author: 'Joem Elias Sanez',
 
@@ -27,7 +25,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/fa8ea052-7e54-48ba-8465-728b2c3cf0ff/joem_elias_sanez_thumbnail.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221210%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221210T164500Z&X-Amz-Expires=86400&X-Amz-Signature=bdfdb6dce2b49f485424d8c998ed1d7b54c7167aca065524b3f436adfe3b24c0&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22joem_elias_sanez_thumbnail.png%22&x-id=GetObject",
+  defaultPageIcon: process.env.NEXT_PUBLIC_PAGE_ICON,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -50,8 +48,8 @@ export default siteConfig({
   pageUrlOverrides: null,
 
   pageUrlAdditions: {
-    '/resume-redux': '8d71ade5bc1d40f58f5bdfd58cef35a3',
-    '/vim-hacks': '303edafc2b074aa9b1ba2a6052807410'
+    '/resume-redux': process.env.NEXT_PUBLIC_RESUME_ID,
+    '/vim-hacks': process.env.NEXT_PUBLIC_VIM_ID
   },
 
   // whether to use the default notion navigation style or a custom one with links to
@@ -61,11 +59,11 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'About',
-      pageId: 'd441caf3bf7a47cc87eccb0da2853eff'
+      pageId: process.env.NEXT_PUBLIC_ABOUT_ID
     },
     {
       title: 'Contact',
-      pageId: 'f086c930ca9a49b8b2a4814ab4da607e'
+      pageId: process.env.NEXT_PUBLIC_CONTACT_ID
     }
   ]
 })
