001041af92c7f404068358ad7d989e9bc2a0e572
Joem Elias Sanez <61286823+israelias@users.noreply.github.com>
2023-08-31 23:03:16 +0800

    feat: customize page head images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,11 +10,22 @@ export default class MyDocument extends Document {
         <Html lang='en'>
           <Head>
             <link rel='shortcut icon' href='/favicon.ico' />
-            <link
+             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='favicon.png'
+              href='/favicon-32x32.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='180x180'
+              href='/apple-touch-icon.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='16x16'
+              href='/favicon-16x16.png'
             />
 
             <link rel='manifest' href='/manifest.json' />
